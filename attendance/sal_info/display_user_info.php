181f3728ef961f45dfff7650837e33c52e406ee5
Manish <manish@excellencetechnologies.in>
2016-09-27 12:43:28 +0530

    Display bank account no and salary of employee.

--- attendance/sal_info/display_user_info.php
+++ attendance/sal_info/display_user_info.php
@@ -5,12 +5,21 @@ ini_set('display_errors', 0);
 require_once ("c-salary.php");
 
 if (isset($_POST['submit'])) {
-    
- 
+
+
     $data = $_POST['user_id'];
-    
+
     $token = $_POST['token'];
-  
+
+    $user_id = Salary::getIdUsingToken($token);
+    $userinfo = Salary::getUserDetail($user_id);
+    if ($userinfo['type'] != "admin" && $userinfo['type'] != "hr") {
+        echo "You are not authorise to access this page";
+        die;
+    }
+
+
+
     $validateToken = Salary::validateToken($token);
 
     if ($validateToken != false) {
@@ -34,40 +43,35 @@ if (isset($_POST['submit'])) {
         //end -- check for token expiry
     }
     if ($validateToken == false) {
-    header("HTTP/1.1 401 Unauthorized");
-    exit;
+   echo "Login Token Expired please login again";
+    die;
 }
+    header("Content-type: text/plain");
+    header("Content-Disposition: attachment; filename=Employee_detail.txt");
+
+    $year = date('Y', strtotime(date('Y-m') . " -1 month"));
+    $month = date('m', strtotime(date('Y-m') . " -1 month"));
+    $ar = array();
+    $ar[] = "Account No\tCurrent Salary\tName";
+    if (sizeof($data) > 0) {
+        foreach ($data as $val) {
+            $s = array();
+            $r1 = Salary::getUserBankDetail($val);
+            $r2 = Salary::getUserManagePayslip($val, $year, $month);
 
 
-        $year = date('Y', strtotime(date('Y-m')." -1 month"));
-        $month = date('m', strtotime(date('Y-m')." -1 month"));
-        $ar= array();
-       if(sizeof($data) > 0){
-           foreach($data as $val){
-               $s = array();
-               $r1 = Salary::getUserBankDetail($val);
-               $r2 = Salary::getUserManagePayslip($val,$year,$month);
-               
-               
-               $s[] = $r1['bank_account_no'];
-               $s[] = $r2['data']['user_data_for_payslip']['net_salary'];
-               $s[] = $r2['data']['user_data_for_payslip']['name'];
-             
-                 $ar[] = implode("\t",$s);
-            
-               
-             
-           }
-       }
-       
-       foreach($ar as $v){
-           echo $v;
-           echo "<br>";
-           
-       }
-       
+            $s[] = $r1['bank_account_no'];
+            $s[] = $r2['data']['user_data_for_payslip']['net_salary'];
+            $s[] = $r2['data']['user_data_for_payslip']['name'];
 
-}
-else{
-    echo "Nononononono";
+            $ar[] = implode("\t", $s);
+        }
+    }
+
+    foreach ($ar as $v) {
+        echo $v;
+        echo "\n";
+    }
+} else {
+    echo "Invalid file access";
 }
