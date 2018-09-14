f1e58efa58a375dc46e12afc07545589a028d3cd
excellencetechnologies <excellencetechnologies08gmail.com>
2018-09-14 13:43:30 +0530

    fix commented points

--- attendance/API_HR/c-holiday.php
+++ attendance/API_HR/c-holiday.php
@@ -50,17 +50,14 @@ trait Holiday {
         return $return;
     }
 
-    public static function dateExist($date){
-
+    public static function isHolidayDateExists($date){        
         $return = false;
-
         $q = "SELECT * from holidays where date = '$date'";
         $runQuery = self::DBrunQuery($q);
         $rows = self::DBfetchRows($runQuery);        
         if(sizeof($rows) > 0){
             $return = true;
         }
-
         return $return;
     }
 
@@ -72,37 +69,26 @@ trait Holiday {
 
         if(!isset($name) || $name == ""){
             $r_data['message'] = "Please provide holiday name.";
-
         } else if (!isset($date) || $date == ""){
             $r_data['message'] = "Please provide a holiday date.";
-
         } else if (!isset($type) || $type == ""){
             $r_data['message'] = "Please provide holiday type.";
-
         } else {
-
             $q = "SELECT * from holidays where id = '$holiday_id'";
             $runQuery = self::DBrunQuery($q);
             $rows = self::DBfetchRows($runQuery);
-
-            $date_exist = self::dateExist($date);
-
+            $date_exist = self::isHolidayDateExists($date);
             if(sizeof($rows) > 0) {
-
                 if($date_exist) {
                     $r_data['message'] = "Duplicate dates not allowed.";
-
                 } else {
                     $q = " UPDATE holidays SET name = '$name', date = '$date', type = '$type' WHERE id = '$holiday_id' ";
                     self::DBrunQuery($q);
                     $r_data['message'] = "Holiday updated successfully.";
-                }                
-                      
+                }                                      
             } else {
-
                 if($date_exist) {
                     $r_data['message'] = "Duplicate dates not allowed.";
-
                 } else {
                     $ins_holiday = array(
                         'name' => $name,
@@ -111,17 +97,14 @@ trait Holiday {
                     );
                     $insert_holiday = self::DBinsertQuery('holidays', $ins_holiday);
                     $r_data['message'] = "Holiday inserted successfully.";
-                }
-                
+                }                
             }
-
         }
-
         $return = [
             'error' => $r_error,
             'data' => $r_data
         ];
-        
+                        
         return $return;
     }
 
